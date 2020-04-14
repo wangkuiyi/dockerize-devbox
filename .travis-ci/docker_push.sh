bbcb188bd3b953abdbf41b783ea35eaebc5f2c97
Yi Wang <yi.wang.2005@gmail.com>
2020-04-14 11:37:02 -0700

    Provide Docker username

--- .travis-ci/docker_push.sh
+++ .travis-ci/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker tag dev cxwangyi/devbox
 docker push cxwangyi/devbox
