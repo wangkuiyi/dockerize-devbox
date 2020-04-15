38dabf2abf079ddf8804c05625532579121fd213
Yi Wang <yi.wang.2005@gmail.com>
2020-04-14 22:11:21 -0700

    Correct pandoc installation

--- dockerize-pandoc/dockerize-pandoc.sh
+++ dockerize-pandoc/dockerize-pandoc.sh
@@ -4,3 +4,5 @@ set -e
 sudo apt-get -qq install -y pandoc graphviz libgraphviz-dev
 sudo pip3 -q install pygraphviz --install-option="--include-path=/usr/include/graphviz" --install-option="--library-path=/usr/lib/graphviz/"
 sudo pip3 -q install pandocfilters
+sudo sh -c "curl --silent https://raw.githubusercontent.com/wangkuiyi/pandoc-with-graphviz-filter/master/pandoc_all.bash > /usr/local/bin/pandoc_all.bash"
+sudo chmod +x /usr/local/bin/pandoc_all.bash
