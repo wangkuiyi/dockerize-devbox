d7cdf0995a0846bf58bd0b8d9c5f872659648b98
Yi Wang <yi.wang.2005@gmail.com>
2020-04-15 01:19:29 -0700

    Revert "Silence docker push"

--- .travis-ci/docker_push.sh
+++ .travis-ci/docker_push.sh
@@ -2,4 +2,4 @@
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker tag dev cxwangyi/devbox
-docker push --silent cxwangyi/devbox
+docker push cxwangyi/devbox
